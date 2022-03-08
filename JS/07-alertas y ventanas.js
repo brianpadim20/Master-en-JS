91f7d44212b5e1fb22c9259135c35c0fb35fb7e7
Brian Patiño <brianpadim@hotmail.com>
2022-03-08 18:50:23 -0500

    Update: new repository for master en JS with a new lesson

---
+++ JS/07-alertas y ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//alertas
+//alert ("Esta es una alerta");
+
+//confirmación
+/*var resultado = confirm ("Está seguro de querer continuar?");
+console.log(resultado); */
+
+//ingreso de datos
+var resultado = prompt ("Que edad tienes?", 0)//el segundo parámetro es opcional
+console.log (resultado);
