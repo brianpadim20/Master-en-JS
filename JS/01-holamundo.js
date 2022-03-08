defe72088f980584caad7d6e71b653213786b05b
Brian <100809806+brianpadim20@users.noreply.github.com>
2022-03-07 19:45:55 -0500

    Add files via upload

---
+++ JS/01-holamundo.js
@@ -0,0 +1,3 @@
+alert("hola mundo con JS")
+document.write ("Hola mundo, desde fichero externo")
+console.log("Hola mundo con JS desde la consola")
---
+++ JS/02-variables.js
@@ -0,0 +1,11 @@
+//Variables
+//Una variable es un contenedor de información
+
+var pais = "Colombia";
+var continente = "América";
+var antiguedad = 2019
+console.log(pais, continente);
+console.log (antiguedad); 
+console.log (typeof(antiguedad))
+var paisYContinente = pais +' '+continente
+console.log(paisYContinente)
