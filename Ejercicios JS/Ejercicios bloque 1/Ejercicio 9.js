3b843710bc1142eeb99bf340f6cabc944943ef13
Brian Patiño <brianpadim@hotmail.com>
2022-03-09 17:28:54 -0500

    fix: completion of excercise 9

--- Ejercicios JS/Ejercicios bloque 1/Ejercicio 9.js
+++ Ejercicios JS/Ejercicios bloque 1/Ejercicio 9.js
@@ -2,6 +2,30 @@
 /*
 Hacer una calculadora que pida dos números por pantalla
 Si se meten mal los #, pedirlos de nuevo,
-Luego que muestre en el cuerpo de la página en una alerta y por consola el resultado de
+Luego que muestre en el cuerpo de la página y por consola el resultado de
 sumar, restar, multiplicar y dividir.
  */
+
+var a = parseInt(prompt("Ingrese el primer número"));
+var b = parseInt(prompt("Ingrese el segundo número"));
+
+var suma = a+b
+var resta = a-b
+var multiplicacion = a*b
+var division = a/b
+
+while(isNaN(a) || isNaN(b)){
+    alert("Los valores ingresados son incorrectos, no sea idiota");
+    var a = parseInt(prompt("Ingrese el primer número"));
+    var b = parseInt(prompt("Ingrese el segundo número"));
+
+}document.write("<p> la suma de " + a + " y " + b + " es: " + suma + "</p><br/>");
+document.write("<p> la resta de " + a + " y " + b + " es: " + resta+ "</p><br/>");
+document.write("<p> la multiplicación de " + a + " y " + b + " es: " + multiplicacion + "</p><br/>");
+document.write("<p> la división de " + a + " y " + b + " es: " + division + "</p><br/>");
+
+console.log("la suma de " + a + " y " + b + " es: " + suma);
+console.log("la resta de " + a + " y " + b + " es: " + resta);
+console.log("la multiplicación de " + a + " y " + b + " es: " + multiplicacion);
+console.log("la división de " + a + " y " + b + " es: " + division);
+
