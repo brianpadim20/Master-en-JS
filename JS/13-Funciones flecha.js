98f6914e59182788b91bc1152900664d3d332396
Brian Patiño <brianpadim@hotmail.com>
2022-03-21 22:35:18 -0500

    update curso de JS until class 49

--- JS/13-Funciones flecha.js
+++ JS/13-Funciones flecha.js
@@ -1,2 +1,14 @@
 'use strict'
 
+function sumame (numero1, numero2, sumaYMuestra, sumaXDos){
+    var sumar = numero1 + numero2;
+    return sumar;
+    sumaYMuestra(sumar);
+    sumaXDos(sumar);
+    
+}sumame(5,7, dato=>{
+    console.log("la suma es: ", dato);
+},dato=>{
+    console.log("La suma por dos es: ", dato*2);
+});
+
---
+++ JS/14-Ambito.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function holaMundo(texto){
+    console.log(texto);
+    console.log(numero.toString());
+    var hola_mundo = "texto de función" /**
+    Las variables locales de una función no se pueden llamar por fuera de las funciones
+    Mas sin embargo, las variables fuera de las funciones, si se pueden llamar dentro de la función
+    */
+
+}var numero = 12;
+var texto="Hola mundo";
+holaMundo(texto);
