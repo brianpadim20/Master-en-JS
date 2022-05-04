544f37bd5ac8f9362a4add214ca4ea7e77c06879
Brian Patiño <brianpadim@hotmail.com>
2022-05-04 13:03:46 -0500

    feat: Completion of excercise 4 (Local storage)

--- Ejercicios JS/Ejercicios bloque 4 (local storage)/Ejercicio 4.js
+++ Ejercicios JS/Ejercicios bloque 4 (local storage)/Ejercicio 4.js
@@ -1,6 +1,36 @@
 'use strict'
 /*
 Crear un formulario que permita añadir películas
-1-Poner encabezado que sea: añade tu película
-
 */
+
+var formulario = document.querySelector("#formPeliculas")
+
+formulario.addEventListener('submit', ()=>{
+    var titulo = document.querySelector('#AddPelicula').value;
+    if(titulo.length>=1){
+        localStorage.setItem(titulo, titulo);
+
+    }
+    
+
+});
+var ul = document.querySelector("#peliculas-list")
+for(var i in localStorage){
+    if(typeof localStorage[i]== 'string'){
+        var li = document.createElement("li");
+        li.append(localStorage[i]);
+        ul.append(li);
+
+    }
+    
+}var formulariob = document.querySelector("#formBorrarPeliculas")
+
+formulariob.addEventListener('submit', ()=>{
+    var titulo = document.querySelector('#BorrarPelicula').value;
+    if(titulo.length>=1){
+        localStorage.removeItem(titulo, titulo);
+
+    }
+    
+
+});
