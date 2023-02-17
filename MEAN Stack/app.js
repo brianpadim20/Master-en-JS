74d9e6584ef9315d2dc8e8e8b368caa1c2a91595
Brian Patiño <brianpadim@hotmail.com>
2023-02-16 22:16:50 -0500

    feat: Completion of backend with nodejs

--- MEAN Stack/app.js
+++ MEAN Stack/app.js
@@ -11,7 +11,15 @@ var project_routes = require('./routes/project');
 app.use(bodyParser.urlencoded({extended:false})); //configuración necesaria para bodyParser
 app.use(bodyParser.json());//cualquier tipo de petición se convierte a JSON
 
-// CORS
+// Configurar cabeceras y CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');/**Cuando se vaya a publicar la aplicación, en lucar de *
+    se tiene que poner la url permitida o los origenes permitidos*/
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //rutas
 app.use('/api',project_routes);
